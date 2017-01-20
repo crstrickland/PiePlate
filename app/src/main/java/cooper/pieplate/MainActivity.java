dbaf7a33713e1d79102a1be6bf29e8be57cab8ab
crstrickland <damakso@gmail.com>
2017-01-20 04:34:37 -0500

    Basic image taking

--- app/src/main/java/cooper/pieplate/MainActivity.java
+++ app/src/main/java/cooper/pieplate/MainActivity.java
@@ -3,6 +3,7 @@ package cooper.pieplate;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,7 +12,11 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -23,7 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button takePictureButton;
     private ImageView imageView;
+    private SurfaceView preview;
+    private SurfaceHolder previewHolder;
+    private boolean inPreview;
+    private boolean cameraConfigured;
+    private Camera camera = null;
     private Uri file;
+
+    //static final int CAMERA_ROTATION = 90;
 
     private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
@@ -33,14 +45,90 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         takePictureButton = (Button) findViewById(R.id.button_image);
         imageView = (ImageView) findViewById(R.id.imageview);
+        preview = (SurfaceView) findViewById(R.id.camera_preview);
 
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             takePictureButton.setEnabled(false);
             ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+        }
+
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        camera = Camera.open();
+        startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        if(inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera=null;
+        inPreview=false;
+
+        super.onPause();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result=null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width<=width && size.height<=height) {
+                if (result==null) {
+                    result=size;
+                }
+                else {
+                    int resultArea=result.width*result.height;
+                    int newArea=size.width*size.height;
+
+                    if (newArea>resultArea) {
+                        result=size;
+                    }
+                }
+            }
+        }
+
+        return(result);
+    }
+
+    private void initPreview(int width, int height) {
+        if (camera!=null && previewHolder.getSurface()!=null) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            }
+            catch (Throwable t) {
+            }
+
+            if (!cameraConfigured) {
+                Camera.Parameters parameters=camera.getParameters();
+                Camera.Size size=getBestPreviewSize(width, height,
+                        parameters);
+
+                if (size!=null) {
+                    parameters.setPreviewSize(size.width, size.height);
+                    camera.setParameters(parameters);
+                    //camera.setDisplayOrientation(CAMERA_ROTATION);
+                    cameraConfigured=true;
+                }
+            }
         }
     }
 
@@ -83,5 +171,32 @@ public class MainActivity extends AppCompatActivity {
         return new File(mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg");
 
     }
+
+    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            // no-op -- wait until surfaceChanged()
+        }
+
+        public void surfaceChanged(SurfaceHolder holder,
+                                   int format, int width,
+                                   int height) {
+            initPreview(width, height);
+            startPreview();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // no-op
+        }
+    };
+
+
+    private void startPreview() {
+        if (cameraConfigured && camera!=null) {
+            camera.startPreview();
+            inPreview=true;
+        }
+    }
+
+
 
 }
